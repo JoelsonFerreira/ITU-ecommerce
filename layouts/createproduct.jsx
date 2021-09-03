8a0a751a47e6c7ed92b0088b04faf5fed241a663
Joelson Ferreira <joelsonferreira@id.uff.br>
2021-09-03 18:42:44 -0300

    create product form is working

--- layouts/createproduct.jsx
+++ layouts/createproduct.jsx
@@ -1,7 +1,52 @@
 import navstyles from "../styles/Navbar.module.css";
 import styles from "../styles/Admin.module.css";
+import { useContext, useState } from "react";
+import { AdminContext } from "../contexts/AdminContext";
 
-function CreateProduct(){
+function CreateProduct() {
+
+    const { token } = useContext(AdminContext);
+
+    const [ name, setName ]         = useState("");
+    const [ price, setPrice ]       = useState("");
+    const [ imageUrl, setImageUrl ] = useState("");
+    const [ quant, setQuant ]       = useState("");
+
+    async function loginSubmit(e) {
+        e.preventDefault();
+
+        const response = await fetch("https://itu-server.herokuapp.com/products/", { 
+            method:"POST", 
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                auth_token: token, 
+                products: {
+                    title: name,
+                    price: Number(price),
+                    quant: Number(quant),
+                    image_url: imageUrl
+                }
+            })
+        });
+
+        setName("");
+        setPrice("");
+        setImageUrl("");
+        setQuant("");
+
+        const status = response.status;
+
+        if(status == 201) {
+            alert("Produto adicionado com sucesso!");
+        } else if(status == 401) {
+            alert("Você não está autenticado!");
+        }
+    }
+
+
     return (
         <>
             <nav className={navstyles.navbar}>
@@ -9,15 +54,15 @@ function CreateProduct(){
                     <h1 className ={navstyles.nav_left_title}>ITU</h1>
                 </div>
             </nav>
-            <main className={styles.container}>
-                <form className={styles.adminform}> 
-                    <input type="text" placeholder="Nome do produto"/>
-                    <input type="text" placeholder="Preço"/>
-                    <input type="text" placeholder="Ícone do jogo"/>
-                    <input type="text" placeholder="Quantidade de Produtos"/>
+            <main className={styles.container} >
+                <form className={styles.adminform} onSubmit={loginSubmit}> 
+                    <input value={name}     onChange={e => setName(e.target.value)}     type="text" placeholder="Nome do produto" />
+                    <input value={price}    onChange={e => setPrice(e.target.value)}    type="text" placeholder="Preço"/>
+                    <input value={imageUrl} onChange={e => setImageUrl(e.target.value)} type="text" placeholder="Ícone do jogo"/>
+                    <input value={quant}    onChange={e => setQuant(e.target.value)}    type="text" placeholder="Quantidade de Produtos"/>
                     <div className={styles.buttonsContainer}>
                         <button className={styles.secondary_button}>Voltar</button>
-                        <button className={styles.primary_button}>Adicionar</button>
+                        <button className={styles.primary_button} type="submit">Adicionar</button>
                     </div>
                 </form>
             </main>
